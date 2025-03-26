ad9653f4256c1bce63673f33e00598b528dfe6dd
NgoTomek <ngotomek@gmail.com>
2025-03-26 14:41:05 +0100

    Create variables.tf

---
+++ infra/terraform/modules/network/variables.tf
@@ -0,0 +1,33 @@
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
+  default     = "question-paper-generator-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
