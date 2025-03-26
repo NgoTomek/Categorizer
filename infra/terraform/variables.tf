59c3b57ad348d596be71ac841f1ee313d681f001
NgoTomek <ngotomek@gmail.com>
2025-03-26 14:39:22 +0100

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS Region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default     = "question-paper-generator"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
